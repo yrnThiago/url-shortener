51b6cdf9234b87e7a59b1a9ad49c319c42ade31b
yrnThiago <thiagodias_123@hotmail.com>
2025-05-09 22:19:53 -0300

    feat: add z form

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,58 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { useState } from "react";
+
+const FormSchema = z.object({
+  url: z.string().url({
+    message: "URL needs to be valid.",
+  }),
+})
 
 function App() {
-  const [url, setUrl] = useState("");
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      url: "",
+    },
+  })
 
-  const submit = () => {
-    setUrl("asd");
-    console.log("OPA");
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(JSON.stringify(data, null, 2))
   }
+
   return (
-    <div className="bg-neutral-950 flex flex-col items-center justify-center min-h-svh">
-    
-      <div className="text-white">
-        <h1>Shortener URLs</h1>
-      </div>
-
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input placeholder="https://www.urlexample.com" />
-        <Button type="submit" onClick={submit}>Confirm</Button>
-      </div>
-
-    </div>
+<div className="bg-neutral-950 flex flex-col items-center justify-center min-h-svh text-white">
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <Input placeholder="example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+</div>
   )
 }
 
-export default App
-
+export default App;
