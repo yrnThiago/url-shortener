4cd6fd14e03eb7a98e5b8d7448d2acffb6e61fec
yrnThiago <thiagodias_123@hotmail.com>
2025-05-10 13:29:57 -0300

    feat: add api conn

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -12,46 +11,73 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-const FormSchema = z.object({
-  url: z.string().url({
-    message: "URL needs to be valid.",
-  }),
-})
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
+
+  const apiEndpoint = "http://localhost:3000/encurtaai"
+
+  const [shortUrl, setShortUrl] = useState("");
+
+  const FormSchema = z.object({
+    full_url: z.string().url({
+      message: "URL needs to be valid.",
+    }),
+  });
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      url: "",
+      full_url: "",
     },
-  })
+  });
+
+  const addNewUrl = async (data) => {
+    try{
+      const [response] = await Promise.all([axios.post(apiEndpoint, data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })]);
+
+      if (response.data.error) {
+        throw new Error(response.data.error)
+      }
+
+      setShortUrl(response.data.message)
+    } catch (error: any) {
+      console.log(error)
+    } finally {
+      console.log("request sent")
+    }
+  }
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(JSON.stringify(data, null, 2))
+    console.log(addNewUrl(data)) 
   }
 
   return (
-<div className="bg-neutral-950 flex flex-col items-center justify-center min-h-svh text-white">
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="url"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>URL</FormLabel>
-              <FormControl>
-                <Input placeholder="example.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-</div>
+    <div className="bg-neutral-950 flex flex-col items-center justify-center min-h-svh text-white">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+            <FormField
+              control={form.control}
+              name="full_url"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>URL</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+    </div>
   )
 }
 
